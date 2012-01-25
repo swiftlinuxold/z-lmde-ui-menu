9f7a41a0cd9167abe970f15de02952fbd61854dd
Jason Hsu <github@jasonhsu.com>
2012-01-24 20:26:47 -0600

    Fixed the icons

--- usr_local_bin/exitswift.sh
+++ usr_local_bin/exitswift.sh
@@ -7,44 +7,50 @@ export Logout='
 <window title="Log Out" window-position="1">
 
 <vbox>
-  <frame>
   <hbox>
 	<vbox>
+	  <frame>
 	  <hbox>
 		<button>
-		<input file>"/usr/share/icons/gTangish-2.0a1/32x32/actions/gnome-lockscreen.png"</input>
+		<input file>"/usr/share/pixmaps/xscreensaver.xpm"</input>
 		<action>xscreensaver-command -lock</action>
                 <action>EXIT:close</action>
 		</button>
 		<text use-markup="true" width-chars="15"><label>"Lock Screen"</label></text>
       </hbox>
+      </frame>
+      <frame>
 	  <hbox>
 		<button>
-		<input file>"/usr/share/icons/gTangish-2.0a1/32x32/apps/gnome-session-reboot.png"</input>
+		<input file>"/usr/share/icons/nuoveXT2/48x48/apps/gnome-session-reboot.png"</input>
 		<action>reboot.sh</action>
 		</button>
 		<text use-markup="true" width-chars="15"><label>"Reboot"</label></text>
 	  </hbox>
+	  </frame>
 	</vbox>
 
 	<vbox>
+	  <frame>
 	  <hbox>
 		<button>
-		<input file>"/usr/share/icons/gTangish-2.0a1/32x32/apps/gnome-session-logout.png"</input>
+		<input file>"/usr/share/icons/nuoveXT2/48x48/apps/gnome-session-logout.png"</input>
 		<action>logouthelper.sh</action>
 		</button>
 		<text use-markup="true" width-chars="15"><label>"Log Out"</label></text>
       </hbox>
+      </frame>
+      <frame>
 	  <hbox>
 		<button>
-		<input file>"/usr/share/icons/gTangish-2.0a1/32x32/apps/gnome-session-halt.png"</input>
+		<input file>"/usr/share/icons/nuoveXT2/48x48/apps/gnome-session-halt.png"</input>
 		<action>shutdown.sh</action>
 		</button>
 		<text use-markup="true" width-chars="15"><label>"Shutdown"</label></text>
 	  </hbox>
+	  </frame>
 	</vbox>
   </hbox>
-  </frame>
 		
   <hbox>
 	<button>
