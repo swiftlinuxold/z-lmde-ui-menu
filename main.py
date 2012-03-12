cf2fc1ef0185ef8a84c60492fb0f03cf87022335
Jason Hsu <github@jasonhsu.com>
2012-03-12 12:55:30 -0500

    Added gtkdialog, xlockmore, and exit-antix

--- main.py
+++ main.py
@@ -29,44 +29,64 @@ else:
 print '================================='
 print 'BEGIN THE MAIN MENU CONFIGURATION'
 
-# Remove menu-xdg (not provided in antiX Linux)
-os.system('apt-get remove -y menu-xdg')
+def install_pkg_antix (name1, name2, url):
+    print 'DOWNLOADING ' + name1 + ' FROM ' + url
+    wget_command = 'wget -nv -nd -nH -r -l1 --no-parent -A '
+    deb_file = name1 + '*' + name2
+    wget_command = wget_command + chr(39) + deb_file + chr(39) + ' '
+    wget_command = wget_command + url
+    print wget_command
+    os.system (wget_command)
+    os.system ('dpkg -i ' + deb_file)
+    os.system ('rm ' + deb_file)
+    os.system ('rm robot*')
 
-# gtkdialog is needed to run exitswift.sh, the script that provides the logout options.
-# MUST download and install gtkdialog, which is not in any of the repositories in sources.list.
-# Therefore, wget is needed to install it directly.
-os.system('wget http://ftp.us.debian.org/debian/pool/main/g/gtkdialog/gtkdialog_0.7.20-4_i386.deb')
-os.system('dpkg -i gtkdialog_0.7.20-4_i386.deb')
-os.system('rm gtkdialog_0.7.20-4_i386.deb')
+# Need for exit-antix (logout options): 
+# gtkdialog, dbus (already installed), udisks (already installed), xlockmore
+install_pkg_antix ('gtkdialog_', '_i386.deb', 'http://ftp.us.debian.org/debian/pool/main/g/gtkdialog/')
+install_pkg_antix ('xlockmore_', '_i386.deb', 'http://ftp.us.debian.org/debian/pool/main/x/xlockmore/')
+install_pkg_antix ('exit-antix_', '.deb', 'http://www.daveserver.info/antiX/main/')
+
+	
+# wget_command = 'wget -nd -nH -r -l1 --no-parent -A '
+# wget_command = wget_command + chr(39) + 'gtkdialog*i386.deb' + chr(39)
+# wget_command = wget_command + ' http://ftp.us.debian.org/debian/pool/main/g/gtkdialog/'
+# os.system(wget_command)
+# wget -nd -nH -r -l1 --no-parent -A 'gtkdialog*i386.deb' http://ftp.us.debian.org/debian/pool/main/g/gtkdialog/
+# wget -nd -r -l1 --no-parent -A 'gtkdialog*i386.deb' http://ftp.us.debian.org/debian/pool/main/g/gtkdialog/
+
+# os.system('wget http://ftp.us.debian.org/debian/pool/main/g/gtkdialog/gtkdialog_0.7.20-4_i386.deb')
+# os.system('dpkg -i gtkdialog_0.7.20-4_i386.deb')
+# os.system('rm gtkdialog_0.7.20-4_i386.deb')
 
 # Allow non-root users to shutdown, reboot, etc.
-os.system('sudo chmod u+s /sbin/poweroff')
-os.system('sudo chmod u+s /sbin/reboot')
+# os.system('sudo chmod u+s /sbin/poweroff')
+# os.system('sudo chmod u+s /sbin/reboot')
 
-import shutil
+# import shutil
 
-dir1 = dir_develop+'/ui-menu/usr_local_bin'
-dir2 = '/usr/local/bin'
+# dir1 = dir_develop+'/ui-menu/usr_local_bin'
+# dir2 = '/usr/local/bin'
 # Copy the logout files to /usr/local/bin
-src = dir1 + '/exitswift.sh'
-dest = dir2 + '/exitswift.sh'
-shutil.copyfile(src, dest)
-os.system ('chmod a+rx ' + dest)
+# src = dir1 + '/exitswift.sh'
+# dest = dir2 + '/exitswift.sh'
+# shutil.copyfile(src, dest)
+# os.system ('chmod a+rx ' + dest)
 
-src = dir1 + '/logouthelper.sh'
-dest = dir2 + '/logouthelper.sh'
-shutil.copyfile(src, dest)
-os.system ('chmod a+rx ' + dest)
+# src = dir1 + '/logouthelper.sh'
+# dest = dir2 + '/logouthelper.sh'
+# shutil.copyfile(src, dest)
+# os.system ('chmod a+rx ' + dest)
 
-src = dir1 + '/reboot.sh'
-dest = dir2 + '/reboot.sh'
-shutil.copyfile(src, dest)
-os.system ('chmod a+rx ' + dest)
+# src = dir1 + '/reboot.sh'
+# dest = dir2 + '/reboot.sh'
+# shutil.copyfile(src, dest)
+# os.system ('chmod a+rx ' + dest)
 
-src = dir1 + '/shutdown.sh'
-dest = dir2 + '/shutdown.sh'
-shutil.copyfile(src, dest)
-os.system ('chmod a+rx ' + dest)
+# src = dir1 + '/shutdown.sh'
+# dest = dir2 + '/shutdown.sh'
+# shutil.copyfile(src, dest)
+# os.system ('chmod a+rx ' + dest)
 
 
 print 'FINISHED THE MAIN MENU CONFIGURATION'
