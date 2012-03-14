1e3ca9f5ea0a79685d4ba8b1f49f79ebb2cd20f0
Jason Hsu <github@jasonhsu.com>
2012-03-14 15:02:21 -0500

    Removed sudo from persist commands

--- usr_local_bin/reboot.sh
+++ usr_local_bin/reboot.sh
@@ -3,7 +3,7 @@
 # Modified to remove the sudo
 
 if which persist-config &> /dev/null; then
-    sudo persist-config --shutdown --command reboot
+    persist-config --shutdown --command reboot
 else
     reboot
 fi
--- usr_local_bin/shutdown.sh
+++ usr_local_bin/shutdown.sh
@@ -3,7 +3,7 @@
 # Modified to remove the sudo
 
 if which persist-config &> /dev/null; then
-    sudo persist-config --shutdown --command halt
+    persist-config --shutdown --command halt
 else
     halt
 fi
